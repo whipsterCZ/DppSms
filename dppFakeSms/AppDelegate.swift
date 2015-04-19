cd1636001e4525069f424e666b0433df96c888a6
WhipsterCZ <whipsterCZ@gmail.com>
2015-04-19 23:57:27 +0200

    app can update on resume

--- dppFakeSms/AppDelegate.swift
+++ dppFakeSms/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-       
+        generateMessages()
         return true
     }
 
@@ -38,12 +38,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        generateMessages()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    
+    func generateMessages()
+    {
+        messages = Message.generate()
+        NSNotificationCenter.defaultCenter().postNotificationName("update", object: nil)
+    }
 
 }
 
--- dppFakeSms/MessagesViewController.swift
+++ dppFakeSms/MessagesViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class MessagesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    let messages = DI.context.appDelegate.messages
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reload", name: "update", object: nil)
+    }
     
     override func viewDidLoad() {
         
@@ -23,18 +27,26 @@ class MessagesViewController: UIViewController, UITableViewDelegate, UITableView
         tableView.allowsSelection = false
         
         
-        
+    }
+    
+    func reload(){
+        tableView.reloadData()
+    }
+    
+    
+    @IBAction func genererateNew(sender: AnyObject) {
+        DI.context.appDelegate.generateMessages()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return messages.count
+        return DI.context.appDelegate.messages.count
     }
     
      func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as TableViewCell
-        let message = messages[indexPath.item]
+        let message = DI.context.appDelegate.messages[indexPath.item]
         cell.dateLabel.text = message.getRecievedAt()
         cell.dateFromLabel.text = message.getFromDay()
         cell.dateToLabel.text = message.getToDay()
